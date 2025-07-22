9055f2448d5fdd8d3766054273a970035d016389
idembele70 <idembele70@gmail>
2025-07-22 21:07:05 +0200

    feat(app): update entry point  and start script in package.json

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const { resolve } = require("path");
 const cookieParser = require("cookie-parser");
 const methodOverride = require("method-override");
 require("./database");
+const { ports } = require(`./environment/${process.env.NODE_ENV}`);
 
 //configuration
 app.set("views", resolve("views"));
@@ -16,5 +17,13 @@ app.use("/static", express.static(resolve("public")));
 app.use(cookieParser());
 app.use(methodOverride("_method"));
 app.use(router);
+
+app.listen(ports, (err) => {
+  if(err){
+    console.error(`Error encoutered during listening to port`, err);
+    return;
+  }  
+  console.log(`Listening on port: ${ports}`)
+})
 
 module.exports = app;
